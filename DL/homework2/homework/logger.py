17ec6fd45edcfa1f39d92c3a84811b626f785c18
JainagAmbati <ajainag@utexas.edu>
2025-02-24 15:04:33 +0000

    update

--- DL/homework2/homework/logger.py
+++ DL/homework2/homework/logger.py
@@ -30,21 +30,26 @@ def test_logging(logger: tb.SummaryWriter):
             dummy_train_accuracy = epoch / 10.0 + torch.randn(10)
 
             logger.add_scalar("train_loss", dummy_train_loss, global_step)
-            
+            # print("hiiiii")
             metrics["train_acc"].extend(dummy_train_accuracy.tolist())
+            # TODO: log train_loss
+            # TODO: save additional metrics to be averaged
+
             global_step += 1
 
-        
+        # TODO: log average train_accuracy
         avg_train_acc = sum(metrics["train_acc"]) / len(metrics["train_acc"])
-        logger.add_scalar("train_accuracy", avg_train_acc, epoch)
+        logger.add_scalar("train_accuracy", avg_train_acc, global_step)
         # example validation loop
         torch.manual_seed(epoch)
         for _ in range(10):
             dummy_validation_accuracy = epoch / 10.0 + torch.randn(10)
             metrics["val_acc"].extend(dummy_validation_accuracy.tolist())
-            
+            # TODO: save additional metrics to be averaged
+
+        # TODO: log average val_accuracy
         avg_val_acc = sum(metrics["val_acc"]) / len(metrics["val_acc"])
-        logger.add_scalar("val_accuracy", avg_val_acc, epoch)
+        logger.add_scalar("val_accuracy", avg_val_acc, global_step)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
